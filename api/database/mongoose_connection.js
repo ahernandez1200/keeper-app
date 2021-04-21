48fa1ff4f405f5ad6ec94fda65b4091586289bf9
ahernandez1200 <tony.6.h@gmail.com>
2021-04-20 17:38:31 -0700

    add module to connect to mongo via mongoose

---
+++ api/database/mongoose_connection.js
@@ -0,0 +1,77 @@
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/keeperPostsDB");
+
+
+//testing connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connection Successful!");
+});
+
+const keeperPostSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const KeeperPost = mongoose.model('KeeperPost', keeperPostSchema);
+
+
+module.exports = {
+    database: mongoose,
+    theModel: KeeperPost
+};
+
+//module.exports = mongoose;
+
+
+
+
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/keeperPostsDB");
+
+
+// //testing connection
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log("Connection Successful!");
+// });
+
+// const keeperPostSchema = new mongoose.Schema({
+//     title: String,
+//     content: String
+// });
+
+// const KeeperPost = mongoose.model('KeeperPost', keeperPostSchema);
+
+// module.exports = mongoose;
+
+
+
+//module.exports.startConnection = function () {
+    //     const mongoose = require('mongoose');
+    //     mongoose.connect("mongodb://localhost:27017/keeperPostsDB");
+    
+    
+    //     //testing connection
+    //     const db = mongoose.connection;
+    //     db.on('error', console.error.bind(console, 'connection error:'));
+    //     db.once('open', function() {
+    //     console.log("Connection Successful!");
+    //     });
+    
+    //     const keeperPostSchema = new mongoose.Schema({
+    //         title: String,
+    //         content: String
+    //     });
+    
+    //     const KeeperPost = mongoose.model('KeeperPost', keeperPostSchema);
+    // }
+
+
+
+//https://stackoverflow.com/questions/52910351/use-mongoose-into-a-express-route-file
+//https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
+//https://stackoverflow.com/questions/16631064/declare-multiple-module-exports-in-node-js
