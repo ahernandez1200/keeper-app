4fa0a1aced12d69eff323965ce7a95fe4437c64e
ahernandez1200 <tony.6.h@gmail.com>
2021-04-25 22:00:48 -0700

    add rendering of database items upon start

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,76 +9,45 @@ function App() {
 
   const [notes, setNotes] = useState([]);
   const [apiResponse, setApiResponse] = useState("");
+  //to hold the number of entries that are in the database upon startup
+  var initialDbSize;
 
-  //useEffect(() => {
-    // fetch("http://localhost:9000/noteStorage/retreive")
-    //     .then(res => res.json())
-    //     .then(res => {
-    //       console.log(res[0]);
-    //       res.forEach(element=>{
-    //         var newNote = {
-    //           title: element.title ,
-    //           content: element.content ,
-    //           identification: element.id
-    //         }
-    //         AddNote(newNote);
-    //       });
-    //     })
-    //     .catch(err => err);
-  //});
+ 
 
-  useEffect(() => {
-    fetch("http://localhost:9000/noteStorage/retreive")
-    .then(res => res.json())
-    .then(res => {
-      console.log(res.length) ;
-      for(var x = 0; x < res.length; x++) {
-          var newNote = {
-          title: res[x].title ,
-          content: res[x].content ,
-          identification: res[x].id
+  const request = async () => {
+    const response = await fetch("http://localhost:9000/noteStorage/retreive");
+    var json = await response.json();
+    //console.log(await jsonR);
+    initialDbSize = json.length;
+    setInitialNotes(json);
+  }
+
+  request();
+
+  /*the objects will be extract from the passed in array and
+    will then be added to notes array with AddInitialNotes*/
+  function setInitialNotes(items) {
+    items.forEach(element=>{
+      var newNote = {
+        title: element.title ,
+        content: element.content ,
+        identification: element.id
         };
-        console.log("x IS EQUAL TO: " + x);
-        console.log(newNote);
-        // addInitialNotes(newNote);
-      }  
-      console.log("FOR LOOP FINISHED.")
-      // res.forEach(element=>{
-      //   var newNote = {
-      //     title: element.title ,
-      //     content: element.content ,
-      //     identification: element.id
-      //   }
-      //   AddInitialNotes(newNote);
-      // });
-    })
-    .catch(err => err);
-  });
-
-  // setInitialNotes();
-
-  // let setInitialNotes = async (url) => {
-
-  //   let response = await fetch(url);
-  //   let results = await response.json();
-  //   let theLength = await results.length;
-
-  //        for(var x = 0; x < await theLength; x++) {
-  //         var newNote = {
-  //         title: results[x].title ,
-  //         content: results[x].content ,
-  //         identification: results[x].id
-  //       };
-  //       AddInitialNotes(newNote);
-  //     }     
-  //   }
+        AddInitialNotes(newNote, initialDbSize);
+    });
+  }
 
 
-  // setInitialNotes("http://localhost:9000/noteStorage/retreive");
-
-  function addInitialNotes(newNote) {
+  function AddInitialNotes(newNote, dbSize) {
     setNotes((prevNotes) => {
-      return [...prevNotes, newNote];
+      /*if we just did return [...prevNotes, newNote], then the
+        page would countinously re-render and keep adding the newNote.
+        Adding this if-else block puts a break on this.
+      */
+      if(dbSize > prevNotes.length)
+        return [...prevNotes, newNote];
+      else
+        return prevNotes;
     });
   }
 
@@ -111,37 +80,14 @@ function App() {
     };
     fetch("http://localhost:9000/deleteNote", requestOptions)
       .then(response => response.text())
-      .then(data => setApiResponse(data));
+      .then(data => setApiResponse(data))
+      .then(initialDbSize--);
 
 
   }
 
 
 
-
-
-
-  // useEffect(() => {
-  //   fetch("http://localhost:9000/testAPI")
-  //       .then(res => res.text())
-  //       .then(res => setApiResponse(res))
-  //       .catch(err => err);
-  // });
-
-  // useEffect(() => {
-  //   // POST request using fetch inside useEffect React hook
-  //   const requestOptions = {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-  //   };
-  //   fetch("http://localhost:9000/", requestOptions)
-  //       .then(response => response.text())
-  //       .then(data => console.log(data));
-
-  // // empty dependency array means this effect will only run once (like componentDidMount in classes)
-  // }, []);
-  
 
 
 
@@ -171,3 +117,112 @@ function App() {
 }
 
 export default App;
+
+
+
+ // useEffect(() => {
+  //   fetch("http://localhost:9000/noteStorage/retreive")
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     console.log(res.length) ;
+  //     for(var x = 0; x < res.length; x++) {
+  //         var newNote = {
+  //         title: res[x].title ,
+  //         content: res[x].content ,
+  //         identification: res[x].id
+  //       };
+  //       console.log("x IS EQUAL TO: " + x);
+  //       console.log(newNote);
+  //       // addInitialNotes(newNote);
+  //     }  
+  //     console.log("FOR LOOP FINISHED.")
+  //     // res.forEach(element=>{
+  //     //   var newNote = {
+  //     //     title: element.title ,
+  //     //     content: element.content ,
+  //     //     identification: element.id
+  //     //   }
+  //     //   AddInitialNotes(newNote);
+  //     // });
+  //   })
+  //   .catch(err => err);
+  // });
+
+  // setInitialNotes();
+
+  // let setInitialNotes = async (url) => {
+
+  //   let response = await fetch(url);
+  //   let results = await response.json();
+  //   let theLength = await results.length;
+
+  //        for(var x = 0; x < await theLength; x++) {
+  //         var newNote = {
+  //         title: results[x].title ,
+  //         content: results[x].content ,
+  //         identification: results[x].id
+  //       };
+  //       AddInitialNotes(newNote);
+  //     }     
+  //   }
+
+
+  // setInitialNotes("http://localhost:9000/noteStorage/retreive");
+
+  // function retrieveInitialNotes() {
+  //   return fetch("http://localhost:9000/noteStorage/retreive")
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     console.log(res.length) ;
+  //     var newArray = [];
+  //     for(var x = 0; x < res.length; x++) {
+  //         var newNote = {
+  //         title: res[x].title ,
+  //         content: res[x].content ,
+  //         identification: res[x].id
+  //       };
+  //       console.log("x IS EQUAL TO: " + x);
+  //       console.log(newNote);
+  //       newArray.push(newNote);
+  //     }  
+  //     console.log("FOR LOOP FINISHED.");
+  //     //console.log(newArray);
+  //     return newArray;
+  //     // res.forEach(element=>{
+  //     //   var newNote = {
+  //     //     title: element.title ,
+  //     //     content: element.content ,
+  //     //     identification: element.id
+  //     //   }
+  //     //   AddInitialNotes(newNote);
+  //     // });
+  //   })
+  //   .catch(err => err);
+  // }
+
+
+  //console.log(retrieveInitialNotes());
+  //console.log("myArr is: " + myArr);
+
+
+  // useEffect(() => {
+  //   fetch("http://localhost:9000/testAPI")
+  //       .then(res => res.text())
+  //       .then(res => setApiResponse(res))
+  //       .catch(err => err);
+  // });
+
+  // useEffect(() => {
+  //   // POST request using fetch inside useEffect React hook
+  //   const requestOptions = {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+  //   };
+  //   fetch("http://localhost:9000/", requestOptions)
+  //       .then(response => response.text())
+  //       .then(data => console.log(data));
+
+  // // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  // }, []);
+  
