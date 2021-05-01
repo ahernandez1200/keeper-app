bc3589caa7be9eb7986fd0b0f488d2f788e49309
ahernandez1200 <tony.6.h@gmail.com>
2021-04-30 21:03:35 -0700

    set-up registration form

--- client/src/components/RegisterPage/RegisterForm.jsx
+++ client/src/components/RegisterPage/RegisterForm.jsx
@@ -3,13 +3,42 @@ import React, { useState, useEffect } from "react";
 
 
 function MyRegisterForm() {
+
+    const [regInfo, setRegInfo] = useState({
+        email: "",
+        password: "",
+        passwordConfirm: ""
+    });
+
+    function handleChange(event) {
+        setRegInfo(previous=>{
+            return(
+                {
+                    ...previous,
+                    [event.target.name]: event.target.value
+                }
+            );
+        });
+        console.log(regInfo);     
+    }
+
+    function handleSubmit(event) {
+        console.log("submitted");
+        event.preventDefault();
+    }
+
+
     return (
         <div>
-            <form className = "container">
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
-                <input type="password" placeholder="Re-type Password"/>
-                <button>Submit</button>
+            <h1 className = "register-header">Register</h1>
+            <form onChange = {handleChange} onSubmit={handleSubmit} className = "container">
+                <input name="email"
+                     type="email" placeholder="Email"/>
+                <input name="password" 
+                    type="password" placeholder="Password"/>
+                <input name="passwordConfirm" 
+                    type="password" placeholder="Confirm Password"/>
+                <button  type="submit">Submit</button>
             </form>
         </div>
     )
