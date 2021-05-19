c13ce4cdbceee3d7c45837f87a92bd03330104a4
ahernandez1200 <tony.6.h@gmail.com>
2021-05-18 18:36:49 -0700

    add exit option for user

--- client/src/components/notesMainPage/Header.jsx
+++ client/src/components/notesMainPage/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function Header() {
+
+function Header(props) {
+
+
   return (
     <header>
       <h1>Keeper</h1>
+      {props.inNotes && <h1 className="logout">logout</h1>}
     </header>
   );
 }
