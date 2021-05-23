ed087b1a8a71f77914c65d1025f005fa91eef86b
ahernandez1200 <tony.6.h@gmail.com>
2021-05-22 19:34:05 -0700

    add onClick to h1

--- client/src/components/notesMainPage/Header.jsx
+++ client/src/components/notesMainPage/Header.jsx
@@ -7,7 +7,7 @@ function Header(props) {
   return (
     <header>
       <h1>Keeper</h1>
-      {props.inNotes && <h1 className="logout">logout</h1>}
+      {props.inNotes && <h1 onClick={props.logoutFunc} className="logout">logout</h1>}
     </header>
   );
 }
